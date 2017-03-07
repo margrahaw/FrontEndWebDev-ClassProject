ddcfb51425dba92f03d5d70215af49fa4782aeff
MARY HAWLEY <MARGRAHAW@GMAIL.COM>
2017-03-07 18:27:50 -0500

    Solution for week 7

--- js/app.js
+++ js/app.js
@@ -47,3 +47,18 @@ function buildTable(section) {
 
    return wrapper;
 }
+
+
+
+function initMap() {
+        var uluru = {lat: 38.258, lng: -85.739};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
